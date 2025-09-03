60e359dc646c1aee7bbc8a50990f3aa2902f9b9d
Rashid1280 <rashidmubasshir@gmail.com>
2025-09-03 10:40:05 +0530

    Prepare app for Render deployment: use env vars, start script

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI || "mongodb://localhost:27017/yourlocaldb";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
